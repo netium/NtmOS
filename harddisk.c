8d34db34bdd0e2d239f697ca34dc430d71a56c40
netium <zb.public@outlook.com>
2021-09-03 23:39:15 +0800

    Start to implement the LBA read

--- harddisk.c
+++ harddisk.c
@@ -1,3 +1,8 @@
+#include "kern_basic.h"
 #include "harddisk.h"
 
 // Implementation refer to https://wiki.osdev.org/ATA_read/write_sectors
+
+int ata_lba_read(void * buff, size_t sector_lba, size_t num_sectors) {
+
+}
