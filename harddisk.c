8329743c6e3599836c664224ddbbd12322450900
netium <zb.public@outlook.com>
2021-09-08 18:33:55 +0800

    In implementing the harddisks detection

--- harddisk.c
+++ harddisk.c
@@ -2,22 +2,83 @@
 #include "kernel_functions.h"
 #include "harddisk.h"
 
+// The whole implemenation is refer to: https://wiki.osdev.org/ATA_PIO_Mode
+
+enum port_offset {
+	DATA_REGISTER_OFFSET = 0,
+	ERROR_REGISTER_OFFSET = 1,
+	FEATURE_REGISTER_OFFSET = 1,
+	SECTOR_COUNT_REGISTER_OFFSET = 2,
+	LBA_LO_REGISTER_OFFSET = 3,
+	SECTOR_NUMBER_REGISTER_OFFSET = 3,
+	LBA_MID_REGISTER_OFFSET = 4,
+	CYLINDER_LOW_REGISTER_OFFSET = 4,
+	LBA_HI_REGISTER_OFFSET = 5,
+	CYLINDER_HIGH_REGISTER_OFFSET = 5,
+	DRIVE_HEAD_REGISTER_OFFSET = 6,
+	STATUS_REGISTER_OFFSET = 7,
+	COMMAND_REGISTER_OFFSET = 8
+};
+
+enum ata_pio_error_register_bit {
+	AMNF = 1 << 0,
+	TKZNF = 1 << 1,
+	ABRT = 1 << 2,
+	MCR = 1 << 3,
+	IDNF = 1 << 4,
+	MC = 1 << 5,
+	UNC = 1 << 6,
+	BBK = 1 << 7
+};
+
+enum ata_pio_status_register_bit {
+	ERR = 1 << 0,	// Indicates an error occurred, Send a new command to clear it (or nuke it with a Software Reset)
+	IDX = 1 << 1,	// Index, always set to zero
+	CORR = 1 << 2,	// Corrected data. Always set to zero
+	DRQ = 1 << 3,	// Set when the drive has PIO data to transfer, or is ready to accept PIO data.
+	SRV = 1 << 4,	// Overlapped Mode Service Request.
+	DF = 1 << 5,	// Drive Fault Error (does not set ERR).
+	RDY = 1 << 6,	// Bit is clear when drive is spun down, or after an error. Set otherwise.
+	BSY = 1 << 7,	// Indicates the drive is preparing to send/receive data (wait for it to clear). In case of 'hang' (it never clears), do a software reset.
+};
+
+#define MAX_N_HDD (4)
+hdd_device_t s_hdd_devices[MAX_N_HDD] = {0};
+
 int ata_detect();
 
 void init_harddisks() {
 	ata_detect();
 
+	k_printf("HDD detection complete!");
+
 	uint8_t buff[512];
-	ata_lba_read(buff, 0, 1);	
+	ata_pio_lba_read(buff, 0, 1);	
 
 	char str[256];
-	for (int i = 0; i < 10; i++) {
+	for (int i = 0; i < 0x10; i++) {
 		k_sprintf(str, "hdd[%x] read: [%x]", i, buff[i]);
 		k_printf(str);
 	}
 }
 
 int ata_detect() {
+	uint16_t scan_ports[] = {0x1f0, 0x170};
+
+	for (int i = 0; i < MAX_N_HDD; i++) {
+		s_hdd_devices[i].is_present = 0;
+	}
+
+	int n = 0;
+	for (int i = 0; i < sizeof(scan_ports) / sizeof(scan_ports[0]); i++) {
+
+	}
+
+
+}
+
+int ata_detect_hdd(uint16_t port, int is_primary, uint16_t *identify_data_buf) {
+	_io_out8(port + DRIVE_HEAD_REGISTER_OFFSET, )
 	_io_out8(0x1f6, 0xa0);
 	_io_out8(0x1f2, 0x00);
 	_io_out8(0x1f3, 0x00);
@@ -46,7 +107,7 @@ int ata_detect() {
 
 	k_printf("start to poll DRQ");
 
-	while (_io_in8(0x1f7 & 0x8));
+	while (_io_in8(0x1f7) & DRQ == 0);
 
 	uint16_t identify_data[256];
 
@@ -54,24 +115,25 @@ int ata_detect() {
 		identify_data[i] = _io_in16(0x1f0); 
 	}
 }
+
 // Implementation refer to https://wiki.osdev.org/ATA_read/write_sectors
 
-int ata_lba_read(uint8_t * buff, size_t sector_lba, size_t num_sectors) {
+int ata_pio_lba_read(uint8_t * buff, size_t abs_lba_sector, size_t num_sectors) {
 	uint16_t * buffw = (uint16_t *)buff;
 
-	_io_out8(0x01f6, (uint8_t)(((sector_lba >> 24) & 0x0f) | 0xe0));	// port to send drive and bit 24-27 of LBA, also set bit 6 for LBA mode
+	_io_out8(0x01f6, (uint8_t)(((abs_lba_sector >> 24) & 0x0f) | 0xe0));	// port to send drive and bit 24-27 of LBA, also set bit 6 for LBA mode
 
 	_io_out8(0x01f2, (uint8_t)num_sectors);	// send number of sectors
 
-	_io_out8(0x01f3, (uint8_t)sector_lba);
+	_io_out8(0x01f3, (uint8_t)abs_lba_sector);
 
-	_io_out8(0x01f4, (uint8_t)(sector_lba >> 8));
+	_io_out8(0x01f4, (uint8_t)(abs_lba_sector >> 8));
 
-	_io_out8(0x01f5, (uint8_t)(sector_lba >> 16));
+	_io_out8(0x01f5, (uint8_t)(abs_lba_sector >> 16));
 
 	_io_out8(0x01f7, 0x20);	// read with retry
 
-	while (_io_in8(0x01f7) == 8);
+	while (_io_in8(0x1f7) & BSY != 0); 
 
 	size_t total_words = num_sectors * 256;	//  256 bytes per sector
 
--- harddisk.h
+++ harddisk.h
@@ -1,7 +1,15 @@
 #ifndef _HARDDISK_H_
 #define _HARDDISK_H_
 
+typedef struct {
+	uint16_t io_ports_base;
+	uint16_t control_port_base;
+	int is_primary;
+	uint8_t is_present;
+	uint8_t identify_words[256];
+} hdd_device_t;
+
 void init_harddisks();
-int ata_lba_read(uint8_t * buff, size_t sector_lba, size_t num_sectors); 
+int ata_pio_lba_read(uint8_t * buff, size_t sector_lba, size_t num_sectors); 
 
 #endif
