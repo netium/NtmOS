ff21bb3e364042c643037b7b1836d1e7b6e59d41
netium <zb.public@outlook.com>
2021-09-14 11:09:54 +0800

    Complete the build_hdd_image.sh

--- build_hdd_image.sh
+++ build_hdd_image.sh
@@ -2,12 +2,31 @@
 
 # Refer to https://wiki.osdev.org/Loopback_Device
 
+if [ -e /var/ntmos/images/hdd.img ]; then
+	rm -fr /var/ntmos/images/hdd.img
+fi
+
 dd if=/dev/zero of=/var/ntmos/images/hdd.img bs=1M count=50
 
-parted /var/ntmos/images/hdd.img mktable msdos
+parted -s /var/ntmos/images/hdd.img mktable msdos
 
 parted -s /var/ntmos/images/hdd.img mkpart primary fat16 1MiB 49MiB
 
-losetup -o1048576 /dev/loop8 /var/ntmos/images/hdd.img
+# Before this the docker need to restart otherwise it will say hdd.img is busy
+# Need to think about how to fix this
+
+loop_device=$(losetup -o1048576 --find --show /var/ntmos/images/hdd.img)
+
+mkdosfs -F 16 -I $loop_device 
+
+mount $loop_device /mnt
+
+if [ -e /var/ntmos/userspace_app/simpleapp/simpleapp.bin ]; then
+	cp /var/ntmos/userspace_app/simpleapp/simpleapp.bin /mnt/init
+fi
+
+umount /mnt
+
+losetup -d $loop_device
 
 
