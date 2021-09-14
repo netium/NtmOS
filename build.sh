573d0b1c144964f00b9fa12971ad0b38fecd7034
netium <zb.public@outlook.com>
2021-09-14 11:22:58 +0800

    Temp comment out the build HDD image

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 
 docker run --rm -v "$(pwd):/var/ntmos" netium/ntmos-build:latest
 
-docker run --rm --privileged=true -v "$(pwd):/var/ntmos" netium/ntmos-build:latest "/usr/bin/bash" "/var/ntmos/build_hdd_image.sh"
+# docker run --rm --privileged=true -v "$(pwd):/var/ntmos" netium/ntmos-build:latest "/usr/bin/bash" "/var/ntmos/build_hdd_image.sh"
 
 exit $?
